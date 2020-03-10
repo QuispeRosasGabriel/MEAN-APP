2991453d302b77b28e6c8c789040af4991d9900c
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-03-09 22:29:19 -0500

    finishing crud methods

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -18,13 +18,35 @@ employeeController.createEmployee = async (req, res) => {
   res.json({ status: "Empleado guardado" });
 };
 
-employeeController.getEmployee = (req, res) => {};
-employeeController.editEmployee = (req, res) => {
+employeeController.getEmployee = async (req, res) => {
+  //Obteniendo del parametro con req.params.id
+  let employeeParam = await Employee.findById(req.params.id);
+  //Devuelves el empleado encontrado
+  res.json(employeeParam);
+};
+employeeController.editEmployee = async (req, res) => {
+  //obtienes el key id del objeto params
+  const { id } = req.params;
+  //Obtiene todos los datos que me esta enviando a traves de angular
+  const newEmployee = {
+    name: req.body.name,
+    position: req.body.position,
+    office: req.body.office,
+    salary: req.body.salary
+  };
+  //$set setea en mongo db los nuevos datos
+  await Employee.findByIdAndUpdate(id, { $set: newEmployee }, { new: true });
+
   res.json({
-    status: "Edit Employee"
+    status: "Empleado actualizado"
   });
 };
 
-employeeController.deleteEmployee = () => {};
+employeeController.deleteEmployee = async (req, res) => {
+  await Employee.findByIdAndDelete(req.params.id);
+  res.json({
+    status: "Empleado eliminado"
+  });
+};
 
 module.exports = employeeController;
